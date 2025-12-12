405d003cb86b2e605578fe8a5960e120d4cd68bf
youssef <youssefelghamour@gmail.com>
2025-12-12 23:14:51 +0100

    1. Build and Seed a CRM Database with GraphQL Integration

--- crm/models.py
+++ crm/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
 
